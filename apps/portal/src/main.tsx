33fe12fcf7fb1ea971d2a017614d80b1304e2d76
Rich Tillman <richtillman@gmail.com>
2021-10-17 23:30:45 -0700

    feat: generate dashboard and home features for portal app

--- apps/portal/src/main.tsx
+++ apps/portal/src/main.tsx
@@ -1,26 +1,38 @@
 import { StrictMode } from 'react'
 import * as ReactDOM from 'react-dom'
-import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter as Router, useHistory } from 'react-router-dom'
-import { Auth0Provider } from '@auth0/auth0-react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { AppState, Auth0Provider } from '@auth0/auth0-react'
+import { ChakraProvider, Skeleton } from '@chakra-ui/react'
+import loadable from '@loadable/component'
+// import { Loader } from '@phc/shared-ui'
+import { createBrowserHistory } from 'history'
 
-import App from './app/app'
+const App = loadable(() => import('./views/App'))
 
-const onRedirectCallback = (appState: { returnTo: string }) => {
-  const history = useHistory()
+export const history = createBrowserHistory()
 
-  history.push(
-    appState && appState.returnTo ? appState.returnTo : window.location.pathname
-  )
+const onRedirectCallback = (appState: AppState) => {
+  // Use the router's history module to replace the url
+  history.replace((appState && appState.returnTo) || window.location.pathname)
 }
 
 ReactDOM.render(
   <StrictMode>
-    <Router>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </Router>
+    <Auth0Provider
+      domain={`${process.env.NX_AUTH0_DOMAIN}`}
+      clientId={`${process.env.NX_AUTH0_CLIENT_ID}`}
+      redirectUri={window.location.origin}
+      audience={process.env.NX_AUTH0_AUDIENCE}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <Router>
+        <ChakraProvider>
+          {/* <Loader> */}
+          <App />
+          {/* </Loader> */}
+        </ChakraProvider>
+      </Router>
+    </Auth0Provider>
   </StrictMode>,
   document.getElementById('root')
 )
