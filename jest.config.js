a519531dcceab2b05879d0b513252dd70a3f0ce2
Rich Tillman <richtillman@gmail.com>
2021-11-04 14:16:43 -0700

    feat: configs

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,23 @@
 const { getJestProjects } = require('@nrwl/jest')
 
 module.exports = {
-  projects: getJestProjects()
+  projects: getJestProjects(),
+  // Jest transformations -- this adds support for TypeScript
+  // using ts-jest
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  // Runs special logic, such as cleaning up components
+  // when using React Testing Library and adds special
+  // extended assertions to Jest
+  setupFilesAfterEnv: [
+    '@testing-library/react/cleanup-after-each',
+    '@testing-library/jest-dom/extend-expect'
+  ],
+  // Test spec file resolution pattern
+  // Matches parent folder `__tests__` and filename
+  // should contain `test` or `spec`.
+  testRegex: '(.*|(\\.|/)(test|spec))\\.{ts,tsx,js,jxs}?$',
+  // Module file extensions for importing
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 }
