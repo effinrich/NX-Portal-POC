69d8a9be16b533182683fd775e2a0473ac6b73ea
Rich Tillman <richtillman@gmail.com>
2021-12-10 18:27:18 -0800

    fix: mock to fix a bunch of tests

--- apps/portal/src/views/Home/index.spec.tsx
+++ apps/portal/src/views/Home/index.spec.tsx
@@ -2,6 +2,9 @@ import { render } from '@testing-library/react'
 
 import Home from '.'
 
+// eslint-disable-next-line react/jsx-no-useless-fragment
+jest.mock('../../components/MapboxGL', () => () => <></>)
+
 describe('Home', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Home coords={{ lat: 0, lng: 0 }} />)
